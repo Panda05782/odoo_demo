3f7306f11290d3d5e5f569505c627434abbf1afa
Panda05782 <panda.seconde.782@gmail.com>
2025-11-12 16:25:34 +0100

    [FIX] change the code 03

--- demo/models/models.py
+++ demo/models/models.py
@@ -18,6 +18,11 @@ class User(models.Model):
         for user in self:
             if user.description and '\n' in user.description:
                 raise ValueError(f'Description must be oneline, got `{user.description}`')
+            
+    def _description_is_one_line(self):
+        for user in self:
+            if user.description and '\n' in user.description:
+                raise ValueError(f'Description must be oneline, got `{user.description}`')
 
 class Hobby(models.Model):
     _name = 'demo.hobby'
