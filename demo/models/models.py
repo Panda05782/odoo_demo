6985b8a4cbf24965d462ff73f4271c21be34803a
Panda05782 <panda.seconde.782@gmail.com>
2025-11-12 16:28:15 +0100

    [FIX] change the code 04

--- demo/models/models.py
+++ demo/models/models.py
@@ -19,6 +19,7 @@ class User(models.Model):
             if user.description and '\n' in user.description:
                 raise ValueError(f'Description must be oneline, got `{user.description}`')
             
+    @api.constrains('description')
     def _description_is_one_line(self):
         for user in self:
             if user.description and '\n' in user.description:
